8c0d2def52292446d6f2a01b5591770cfa968c6f
Gustavo Sanchez <gs643290@gmail.com>
2022-09-24 16:54:17 -0500

    AngularMiniProyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.module';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-Empleados';
+  titulo = 'Listado Empleados';
+  empleados:Empleado[]=[
+    new Empleado("Gustavo","Sanchez","Analista De Sistemas",5000),
+    new Empleado("Isabel","Sanchez","Jefa De Compras",8000),
+    new Empleado("Gino","Gonzales","Jefe De Sistemas",10000),
+  ];
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0.0;
+  agregarEmpleado()
+  {
+    let miEmpleado = new Empleado (this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario)
+    this.empleados.push(miEmpleado);
+  }
+
+
 }
